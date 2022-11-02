7cdfc494a660c85c09f0dfb04bc51b20c1b13cbf
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-11-02 07:45:32 +0000

    add Trend Micro Agent Layer

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -14,7 +14,19 @@ export class AppStack extends cdk.Stack {
       handler: 'index.handler',
       code: lambda.Code.fromAsset('lambda/')
     });
-
+    
+    
+    // Agent Layer 추가
+    const agentArn = 'arn:aws:lambda:ap-northeast-1:800880067056:layer:CloudOne-ApplicationSecurity-nodejs14_x:1';
+    const agentLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'TrendMicroAgent', agentArn);
+    fn.addLayers(agentLayer)
+    
+    // 환경 변수 추가
+    fn.addEnvironment('AWS_LAMBDA_EXEC_WRAPPER', '/opt/trend_app_protect');
+    fn.addEnvironment('TREND_AP_HELLO_URL', 'https://agents.jp-1.application.cloudone.trendmicro.com/');
+    fn.addEnvironment('TREND_AP_KEY', 'bedb1304-9176-4191-97d9-1972d4112303');
+    fn.addEnvironment('TREND_AP_SECRET', 'aef3dc4a-07b7-4a50-be8b-27a987ca8a0d');
+    
     // The code that defines your stack goes here
 
     // example resource
